9a53cc81ab098f1f00160a4d0124d2f29cb6f473
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-08-06 21:17:45 +0100

    Added my route in my products store also

--- LearnlyApp Edtech Frontend/src/stores/products.js
+++ LearnlyApp Edtech Frontend/src/stores/products.js
@@ -1,3 +1,4 @@
+// productsStore.js
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
@@ -13,24 +14,68 @@ if (token) {
 
 export const useProductsStore = defineStore('products', {
   state: () => ({
-    products: []
+    products: [],
+    product: {},
+    loading: false,
+    error: null
   }),
   actions: {
     async fetchProducts() {
       try {
+        this.loading = true
         const response = await api.get('/product/products')
         this.products = response.data
+        this.loading = false
       } catch (error) {
-        console.error(error)
+        this.error = error
+        this.loading = false
+      }
+    },
+    async fetchProduct(id) {
+      try {
+        this.loading = true
+        const response = await api.get(`/product/products/${id}`)
+        this.product = response.data
+        this.loading = false
+      } catch (error) {
+        this.error = error
+        this.loading = false
       }
     },
     async addProduct(newProduct) {
       try {
+        this.loading = true
         const response = await api.post('/product/createProduct', newProduct)
         this.products.push(response.data)
-        alert('Product added successfully!')
+        this.loading = false
+        alert('Product Added SuccessFully')
       } catch (error) {
-        console.error(error)
+        this.error = error
+        this.loading = false
+      }
+    },
+    async updateProduct(id, updatedProduct) {
+      try {
+        this.loading = true
+        const response = await api.put(`/product/products/${id}`, updatedProduct)
+        const index = this.products.findIndex((product) => product.id === id)
+        this.products.splice(index, 1, response.data)
+        this.loading = false
+      } catch (error) {
+        this.error = error
+        this.loading = false
+      }
+    },
+    async deleteProduct(id) {
+      try {
+        this.loading = true
+        await api.delete(`/product/products/${id}`)
+        const index = this.products.findIndex((product) => product.id === id)
+        this.products.splice(index, 1)
+        this.loading = false
+      } catch (error) {
+        this.error = error
+        this.loading = false
       }
     }
   }
